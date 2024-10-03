b6b3b79fa845e1bd0600d033f2c48014c08a0c79
jessyjemz14@gmail.com <145030907+Jessy-tech14@users.noreply.github.com>
2024-10-03 16:00:01 +0300

    The class includes a constructor that takes 4 parameters: student_name and registration_number age and course.
    When you run the code, it will create 4
    student instances and print out their information
    as recorded in the parameters.

---
+++ assgn.py
@@ -0,0 +1,23 @@
+class Student:
+    def __init__(self, student_name, registration_number, age, course):
+        self.student_name = student_name
+        self.registration_number = registration_number
+        self.age = age
+        self.course = course
+
+    def display_info(self):
+        print(f"Student Name: {self.student_name}")
+        print(f"Registration Number: {self.registration_number}")
+        print(f"Age: {self.age}")
+        print(f"Course : {self.course}")
+        
+student1 = Student("Laker Jessy", "S23B13/029", 20, "Information Technology")
+student2 = Student("Oliver Mugisa", "S23B13/889", 22, "Computer Science")
+student3 = Student("Peter Oppong", "S23B13/082", 21, "Electrical Engineering")
+student4 = Student("Nancy Eze", "S23B13/009", 20, "Data Science")
+
+
+student1.display_info()
+student2.display_info()
+student3.display_info()
+student4.display_info()
